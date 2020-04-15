f24b7c5cbaa174e701ad33910e65db6b8f077db6
Patrick Baldwin <patrick@patrickalanbaldwin.com>
2020-04-15 09:06:10 -0700

    No path prefixes.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 const path = require("path")
 
 module.exports = {
-  pathPrefix: "/pog-site",
   siteMetadata: {
     title: `Planet of Giants`,
     description: `The official website of Planet of Giants. Get the latest news, music, and information on the band.`,
