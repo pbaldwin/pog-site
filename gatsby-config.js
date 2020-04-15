6ea4453f4ff195ca7150cb77e93a7e31fac43767
Patrick Baldwin <patrickalanbaldwin@gmail.com>
2020-04-15 09:56:52 -0700

    Adds flamingo icon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
         background_color: `#2f1c30`,
         theme_color: `#2f1c30`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/flamingo-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
